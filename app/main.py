a43f2356fb2995ce2db082e10d00cfee91a1539b
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-10-18 10:13:59 -0400

    1

--- app/main.py
+++ app/main.py
@@ -75,13 +75,12 @@ async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db
 
 cookie_transport = CookieTransport(cookie_max_age=3600)
 
-def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+jwt_strategy = JWTStrategy(secret=SECRET, lifetime_seconds=3600)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
     transport=cookie_transport,
-    get_strategy=get_jwt_strategy,
+    get_strategy=lambda: jwt_strategy,
 )
 
 fastapi_users = FastAPIUsers[User, int](
