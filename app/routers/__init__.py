1df115e06798a495711ff31ab1a99169fbc7aa78
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-11-24 10:34:56 -0500

    1

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -5,9 +5,11 @@ API route handlers organized by domain.
 from .templates import router as templates_router
 from .locations import router as locations_router
 from .admin import router as admin_router
+from .logs import router as logs_router
 
 __all__ = [
     "templates_router",
     "locations_router",
     "admin_router",
+    "logs_router",
 ]
