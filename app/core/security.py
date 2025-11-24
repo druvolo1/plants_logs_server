849f69386e464fb289bb94c39a787cdf37a282be
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-11-23 20:55:59 -0500

    1

--- app/core/security.py
+++
@@ -1,7 +0,0 @@
-"""Security utilities and JWT strategy."""
-from fastapi_users.authentication import JWTStrategy
-from .config import SECRET
-
-def get_jwt_strategy() -> JWTStrategy:
-    """Get JWT authentication strategy."""
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600 * 24 * 7)  # 7 days
